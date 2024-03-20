eaf015f938dbf7f02695988f53885e87e5553e55
Jimin Kim <0305jimin@gmail.com>
2024-03-20 19:43:57 +0900

    Feat: allowing users to get tweets and a single tweet
    alike GET /api/v1/tweets, GET /api/v1/tweet/:id from REST API

--- tweetQL/server.js
+++ tweetQL/server.js
@@ -1,1 +1,25 @@
 import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+    type User {
+        id: ID
+        username: String
+    }
+
+    type Tweet {
+        id: ID
+        text: String
+        author: User
+    }
+
+    type Query {
+        allTweets: [Tweet] # similar to GET /api/v1/tweets from REST API
+        tweet(id: ID): Tweet # similar to GET /api/v1/tweet/:id from REST API url variable from REST ~= argument from gql
+    }
+`;
+
+const server = new ApolloServer({typeDefs});
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
