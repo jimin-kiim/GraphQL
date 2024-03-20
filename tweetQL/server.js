6fd8b10c698f101534011ee65311a9fa9cf5b53e
Jimin Kim <0305jimin@gmail.com>
2024-03-20 22:31:23 +0900

    Feat: allowing users to post, delete tweets and adding making the fileds
    non nullable

--- tweetQL/server.js
+++ tweetQL/server.js
@@ -1,24 +1,29 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
-    type User {
-        id: ID
-        username: String
-    }
+  type User {
+    id: ID
+    username: String
+  }
 
-    type Tweet {
-        id: ID
-        text: String
-        author: User
-    }
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User!
+  }
 
-    type Query {
-        allTweets: [Tweet] # similar to GET /api/v1/tweets from REST API
-        tweet(id: ID): Tweet # similar to GET /api/v1/tweet/:id from REST API url variable from REST ~= argument from gql
-    }
+  type Query { # GET (REST API)
+    allTweets: [Tweet!]! # similar to GET /api/v1/tweets from REST API
+    tweet(id: ID!): Tweet! # similar to GET /api/v1/tweet/:id from REST API url variable from REST ~= argument from gql
+  }
+
+  type Mutation { # POST, PUT, DELETE (REST API)
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
 `;
 
-const server = new ApolloServer({typeDefs});
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
