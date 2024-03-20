b4487be736de45a242e5789fa75b161bdaed3618
Jimin Kim <0305jimin@gmail.com>
2024-03-20 22:58:15 +0900

    Fix: refining some nullable and non nullable fields and adding comments

--- tweetQL/server.js
+++ tweetQL/server.js
@@ -2,8 +2,10 @@ import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String!
   }
 
   type Tweet {
@@ -13,8 +15,8 @@ const typeDefs = gql`
   }
 
   type Query { # GET (REST API)
-    allTweets: [Tweet!]! # similar to GET /api/v1/tweets from REST API
-    tweet(id: ID!): Tweet! # similar to GET /api/v1/tweet/:id from REST API url variable from REST ~= argument from gql
+    allTweets: [Tweet!]! # similar to GET /api/v1/tweets from REST API # it always should be a list and the inside of the list always should be Tweet. empty can be allowed
+    tweet(id: ID!): Tweet # similar to GET /api/v1/tweet/:id from REST API url variable from REST ~= argument from gql
   }
 
   type Mutation { # POST, PUT, DELETE (REST API)
