6cefb5a92cf2e1af6f1bdb3209d4d35ea5ebd542
Jimin Kim <0305jimin@gmail.com>
2024-03-21 20:23:24 +0900

    Feat: adding typeDefs movie and Query
    using previously existing REST API into GraphQL

---
+++ movieQL/server.js
@@ -0,0 +1,38 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+    type Movie {
+        id: Int!
+        url: String!
+        imdb_code: String!
+        title: String!
+        title_english: String!
+        title_long: String!
+        slug: String!
+        year: Int!
+        rating: Float!
+        runtime: Float!
+        genres: [String]!
+        summary: String!
+        description_full: String!
+        synopsis: String!
+        yt_trailer_code: String!
+        language: String!
+        background_image: String!
+        background_image_original: String!
+        small_cover_image: String!
+        medium_cover_image: String!
+        large_cover_image: String!
+  }
+
+  type Query { 
+    allMovies: [Movie!]!
+    movie(id: String!): Movie
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
