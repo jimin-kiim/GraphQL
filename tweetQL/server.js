e0e4adffa7b67a2f0a02c75d1be8cc63b2957710
Jimin Kim <0305jimin@gmail.com>
2024-03-21 17:30:42 +0900

    Feat: implementing Type Resolver to allow to get fullName even though
    the data contains only first and last name

--- tweetQL/server.js
+++ tweetQL/server.js
@@ -11,12 +11,20 @@ let tweets = [
   }
 ];
 
+let users = [
+  {
+    id: "1",
+    firstName: "jimin",
+    lastName: "kim"
+  }
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
     firstName: String!
     lastName: String!
+    fullName: String! # a field that doesn't exist in the data but the resolver does. So it works
   }
 
   type Tweet {
@@ -26,6 +34,7 @@ const typeDefs = gql`
   }
 
   type Query { # GET (REST API)
+    allUsers: [User!]!
     allTweets: [Tweet!]! # similar to GET /api/v1/tweets from REST API # it always should be a list and the inside of the list always should be Tweet. empty can be allowed
     tweet(id: ID!): Tweet # similar to GET /api/v1/tweet/:id from REST API url variable from REST ~= argument from gql
   }
@@ -37,32 +46,43 @@ const typeDefs = gql`
 `;
 
 const resolvers = {
+  User: {
+    fullName({ firstName, lastName }) {
+      // opening the root argument and getting two infos
+      return `${firstName} ${lastName}`;
+    }
+  },
   // resolver for the fields
   Query: {
+    allUsers() {
+      return users;
+    },
+
     allTweets() {
       return tweets;
     },
 
-    tweet(_, {id}) { // root, args. {id} is opening up the args and getting id.
+    tweet(_, { id }) {
+      // root, args. {id} is opening up the args and getting id.
       // when the tweet field in Query is reduired, this function is called.
-      return tweets.find(tweet => tweet.id === id);
+      return tweets.find((tweet) => tweet.id === id);
     }
   },
-  
+
   Mutation: {
-    postTweet(_, {text, userId}) {
+    postTweet(_, { text, userId }) {
       const newTweet = {
         id: tweets.length + 1,
-        text,
+        text
       };
       tweets.push(newTweet);
       return newTweet;
     },
 
-    deleteTweet(_, {id}) {
-      const tweet = tweets.find(tweet =>  tweet.id === id);
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
       if (!tweet) return false;
-      tweets = tweets.filter(tweet => tweet.id !== id);
+      tweets = tweets.filter((tweet) => tweet.id !== id);
       return true;
     }
   }
